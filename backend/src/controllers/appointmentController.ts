cb968e5eb5d825ed27ec1328b107528e79a77912
Ahmed I. Yassen <ahmed.ibrahim.yassen@gmail.com>
2022-07-20 07:01:58 +0200

    Finish Create Appointment functionality

--- backend/src/controllers/appointmentController.ts
+++ backend/src/controllers/appointmentController.ts
@@ -51,4 +51,76 @@ export default class AppointmentController {
       next(error);
     }
   };
+
+  createAppointment = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      if (this.isOffDay(new Date(req.body.date)))
+        throw new Error("The clinic is closed on Friday & Saturday");
+
+      const doctor = await Doctor.findById(req.body.doctorId);
+      if (!doctor) throw new Error("There is no doctor with that ID");
+
+      const currentDate = new Date();
+      const requestDate = new Date(req.body.date);
+      if (moment(requestDate).isBefore(currentDate))
+        throw new Error("Cannot set an appointment on a day from the past!");
+
+      /* ADDING APPOINTMENT
+       -Cannot add two Appointments at the same time to one doctor.
+       -Clinc Working hours start from 10 AM to 6 PM (No Work on Friday & Saturday) 
+       -Doctors get 16 appointments a day, half an hour each.
+       */
+
+      const doctorAppointments = await this.getDoctorAppointmentsOnDay(
+        new Date(req.body.date),
+        req.body.doctorId
+      );
+
+      let appointmentDate: Date;
+      switch (doctorAppointments.length) {
+        case 0:
+          //-Doctor has no appointments yet on this day, create first appointment of the day at 10:00:00 AM.
+          appointmentDate = moment(new Date(req.body.date))
+            .hour(10)
+            .minute(0)
+            .second(0)
+            .toDate();
+          break;
+
+        case 16:
+          return res.json({
+            success: "false",
+            msg: "This day is full, Try the next day!",
+          });
+
+        default:
+          //-Doctor has 1 or more appointments, get last one and add 30minutes
+          const lastAppointmentThisDay =
+            doctorAppointments[doctorAppointments.length - 1].date;
+          //-Date is returned as GMT+2, Subtract 2 hours to convert it to GMT
+          appointmentDate = moment(lastAppointmentThisDay)
+            .subtract(2, "hours")
+            .add(30, "minutes")
+            .toDate();
+      }
+
+      let requestData = { ...req.body };
+      requestData.date = appointmentDate;
+      const newAppointment = await new Appointment(requestData);
+      await newAppointment.save();
+
+      res.json({
+        success: true,
+        msg: `Appointment created successfully at ${appointmentDate
+          .toUTCString()
+          .replace(" GMT", "")}`,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
 }
--- backend/src/routes/appointmentRoute.ts
+++ backend/src/routes/appointmentRoute.ts
@@ -6,15 +6,40 @@ import validationMW from "../middlewares/validationMW";
 const router = Router();
 const appointmentController = new AppointmentController();
 
-router.route("/appointments").get(
-  [
-    body("doctorId")
-      .isMongoId()
-      .withMessage("Doctor's ID Should be a valid MongoID!"),
-    // body("date").isDate().withMessage("Please provide a valid date!"),
-  ],
-  validationMW,
-  appointmentController.checkDoctorAppointmentsByDay
-);
+router
+  .route("/appointments")
+  .get(
+    [
+      body("doctorId")
+        .isMongoId()
+        .withMessage("Doctor's ID Should be a valid MongoID!"),
+      body("date").isDate().withMessage("Please provide a valid date!"),
+    ],
+    validationMW,
+    appointmentController.checkDoctorAppointmentsByDay
+  )
+  .post(
+    [
+      body("patientId")
+        .isMongoId()
+        .withMessage("Patient's ID Should be a valid MongoID!"),
+      body("doctorId")
+        .isMongoId()
+        .withMessage("Doctor's ID Should be a valid MongoID!"),
+      body("specialtyId")
+        .isMongoId()
+        .withMessage("Specialty's ID Should be a valid MongoID!"),
+      body("employeeId")
+        .isMongoId()
+        .withMessage("Employee's ID Should be a valid MongoID!"),
+      body("type")
+        .toLowerCase()
+        .isIn(["consultation", "followup"])
+        .withMessage("Type should be text, consultation or followup"),
+      body("date").isDate().withMessage("Please provide a valid date format!"),
+    ],
+    validationMW,
+    appointmentController.createAppointment
+  );
 
 export default router;
