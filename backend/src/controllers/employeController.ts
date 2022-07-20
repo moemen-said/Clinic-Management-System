0e888d7c6f046de004e2ee515616b55a3048030d
Ahmed-Basher1 <a7medbasher811@gmail.com>
2022-07-20 20:04:49 +0200

    adding validation for doctors and employee

--- backend/src/controllers/employeController.ts
+++ backend/src/controllers/employeController.ts
@@ -4,7 +4,7 @@ require("../models/user")
 
 import Employee from '../models/employee';
 
-export default class doctorController{
+export default class EmployeeController{
     constructor(){ }
     createEmployee (req:Request,res:Response,next:NextFunction) {
         const { userId,name } = req.body;
@@ -21,5 +21,36 @@ export default class doctorController{
             .then((emp) => res.status(201).json({ emp }))
             .catch((error) => res.status(500).json({ error }));
     }
+
+
+    getAllEmployee (req:Request,res:Response,next:NextFunction) {
+        return Employee.find({})
+            .populate({path:"userId",options:{sort:{name:-1}}})
+            .then((data) => (data ? res.status(200).json({ data }) : res.status(404).json({ message: 'not found' })))
+            .catch((error)=>next(error));
+    }
+
+    async updateEmployee(req:Request,res:Response,next:NextFunction){
+        const data:any = await Employee.findOne({_id:req.params.id})
+          if(data){
+           for (const property in req.body) {
+               data[property] = req.body[property];
+             }
+           await  data.save();
+           res.status(200).json({Message:"updated",data})
+   
+          }
+       }
+
+       async deleteEmployee(req:Request,res:Response,next:NextFunction){
+
+        try {
+            const data = await Employee.findByIdAndDelete({_id:req.params.id})
+            return res.status(200).json({Message:"deleted Successful",data})
+        } catch (error) {
+                next(error)
+        }
+
+       }
 
 }
--- backend/src/routes/doctorRoute.ts
+++ backend/src/routes/doctorRoute.ts
@@ -1,5 +1,7 @@
-import { Router,Request,Response } from "express";
+import { Router,Request,Response,NextFunction } from "express";
 import doctorController from "../controllers/doctorController";
+import { body, param } from "express-validator";
+import validationMW from "../middlewares/validationMW";
 
 const DoctorController = new doctorController();
 const router = Router();
@@ -7,9 +9,44 @@ const router = Router();
 router.get('/doctors',DoctorController.getAllDoctors);
 router.get('/doctors/:id',DoctorController.getDoctorById);
 
-router.post('/doctors',DoctorController.createDoctor);
+router.post('/doctors',[
+    body("userId").isMongoId().withMessage("UserId should be a valid MongoID."),
+    body("name").notEmpty().isString().withMessage("name should be a stirng and not empty.")
+    // .custom(value => {
+        
+    //     try {
+    //         const data:any = Doctor.findOne({name: value})
+    //         if(!data){
+    //             console.log(data)
+    //             return Promise.resolve()
+    //         }
+    //         else{
+    //             return Promise.reject('Name already taken')
+    //         }
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // })
+    //     return Doctor.findOne({name: value})
+    //        .then((data) => {
+            
+    //           return Promise.reject('Name already taken')
+    //        }).catch(err=>console.log(err))
+           
+    //  })
+,
+    body("specialtyId").isMongoId().withMessage("specialtyId should be a valid MongoID."),
+    body("examinationPrice").isNumeric().withMessage("examinationPrice should be number!"),
+],validationMW,DoctorController.createDoctor);
 
-router.put('/doctors/:id',DoctorController.updateDoctor);
-router.delete('/doctors/:id',DoctorController.deleteDoctor);
+router.put('/doctors/:id',[
+    param("id").isMongoId().withMessage("id should be a valid MongoID."),
+    body("userId").isMongoId().withMessage("UserId should be a valid MongoID."),
+    body("name").isString().withMessage("Name should be text!"),
+    body("specialtyId").isMongoId().withMessage("specialtyId should be a valid MongoID."),
+    body("examinationPrice").isNumeric().withMessage("examinationPrice should be number!"),
+],validationMW,DoctorController.updateDoctor);
+router.delete('/doctors/:id',param("id").isMongoId().withMessage("params id should be mongo id"),
+validationMW,DoctorController.deleteDoctor);
 
 export default router;
